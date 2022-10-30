0bcbeb60c583277258ee87e36dc529d13355f247
arnab <arnabb.bannerjee@gmail.com>
2022-10-30 21:14:31 +0530

    google login and localstorage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
-import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logowhite.png';
-
+import React, { useEffect } from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+import config from "../config.json"
+import { gapi } from "gapi-script";
 
 const Login = () => {
+  const responseGoogle = (response) => {
+    if (!response || !response.error) {
+      localStorage.setItem('user', JSON.stringify(response.profileObj));
+      const { name, googleId, imageUrl } = response.profileObj;
+      console.log(name, googleId, imageUrl);
+    }
+  };
+
+  gapi.load("client:auth2", () => {
+    gapi.auth2.init({clientId: config.GOOGLE_API_TOKEN})
+  });
+  
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
@@ -22,19 +35,25 @@ const Login = () => {
 
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
-            <img src={logo} width="130px" alt=""/>
+            <img src={logo} width="130px" alt="" />
           </div>
 
           <div className="shadow-2xl">
             <GoogleLogin
+              clientId={config.GOOGLE_API_TOKEN}
               render={(renderProps) => (
                 <button
                   type="button"
                   className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
                 >
-                  <FcGoogle className="mr-4" /> Sign in with google
+                  <FcGoogle className="mr-4" /> Sign in with Google
                 </button>
               )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
             />
           </div>
         </div>
